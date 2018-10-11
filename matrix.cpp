698b684a4ebe577469077ed183724da4a6361f50
jonchiu94 <jchiu84@my.bcit.ca>
2018-10-10 23:59:36 -0700

    Changing matrix

--- matrix.cpp
+++ matrix.cpp
@@ -3,3 +3,46 @@
 //
 
 #include "matrix.hpp"
+#include <math.h>
+#include <iostream>
+using namespace std;
+
+matrix::matrix(){
+    matrix_array[1]={0.0};
+}
+matrix::matrix(int size){
+    matrix_array[size*size]={0.0};
+}
+matrix::matrix(int r, int c){
+    matrix_array[r*c]={0.0};
+}
+matrix::matrix(double input[]){
+    size =0;
+    while(input[size]){
+        size++;
+    }
+    double sr = sqrt(size);
+    if((sr-floor(sr))==0){
+        throw "size of array is not a perfect square";
+    }
+}
+void matrix::set_value(int row, int column, double value){
+
+}
+void get_value(int row, int column);
+void clear();
+~matrix();
+friend std::ostream &operator<< (std::ostream &os, const matrix &matrix);
+friend bool operator== (const matrix &left_matrix, const matrix &right_matrix);
+friend bool operator!= (const matrix &left_matrix, const matrix &right_matrix);
+matrix& operator++();
+matrix operator++(int);
+matrix& operator--();
+matrix operator--(int);
+matrix& operator= (matrix right);
+matrix& operator+= (const matrix& right);
+friend matrix operator+ (matrix left, const matrix& right);
+matrix& operator-= (const matrix& right);
+friend matrix operator- (matrix left, const matrix& right);
+matrix& operator*= (const matrix& right);
+friend matrix operator* (matrix left, matrix right);
--- matrix.hpp
+++ matrix.hpp
@@ -8,18 +8,31 @@ class matrix {
 private:
     int row;
     int column;
+    int size;
+    double matrix_array[];
 public:
     matrix();
     matrix(int size);
     matrix(int r, int c);
-    matrix(double array[]);
+    matrix(double input[]);
     void set_value(int row, int column, double value);
     void get_value(int row, int column);
     void clear();
     ~matrix();
     friend std::ostream &operator<< (std::ostream &os, const matrix &matrix);
-
-
+    friend bool operator== (const matrix &left_matrix, const matrix &right_matrix);
+    friend bool operator!= (const matrix &left_matrix, const matrix &right_matrix);
+    matrix& operator++();
+    matrix operator++(int);
+    matrix& operator--();
+    matrix operator--(int);
+    matrix& operator= (matrix right);
+    matrix& operator+= (const matrix& right);
+    friend matrix operator+ (matrix left, const matrix& right);
+    matrix& operator-= (const matrix& right);
+    friend matrix operator- (matrix left, const matrix& right);
+    matrix& operator*= (const matrix& right);
+    friend matrix operator* (matrix left, matrix right);
 };
 
 
